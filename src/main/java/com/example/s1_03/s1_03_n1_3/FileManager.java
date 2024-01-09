121d3355ede1446311198349be7a405b51902750
florinato <91286218+florinato@users.noreply.github.com>
2024-01-09 12:52:38 +0100

    mas correcciones

--- src/main/java/com/example/s1_03/s1_03_n1_3/FileManager.java
+++ src/main/java/com/example/s1_03/s1_03_n1_3/FileManager.java
@@ -3,7 +3,7 @@ package com.example.s1_03.s1_03_n1_3;
 import java.io.*;
 
 public class FileManager {
-    private static final String NOMBRE_ARCHIVO = "classificacio.txt";
+    private static final String NOMBRE_ARCHIVO = "src/main/java/com/example/s1_03/s1_03_n1_3/classificacio.txt";
 
     public static void guardarPuntuacion(String nombreUsuario, int puntos) throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(NOMBRE_ARCHIVO, true))) {
